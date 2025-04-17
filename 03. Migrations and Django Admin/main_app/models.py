03fed00e74ab4b54fd2edbb42aa07d46c3228d8c
Vselenis <77938348+Vselenis@users.noreply.github.com>
2025-04-17 22:13:38 +0300

    2nd

--- 03. Migrations and Django Admin/main_app/models.py
+++ 03. Migrations and Django Admin/main_app/models.py
@@ -7,3 +7,9 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=100, null=True, blank=True)
     supplier = models.CharField(max_length=150, null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_edited_on = models.DateTimeField(auto_now=True)
+    count = models.PositiveIntegerField(default=0)
+    category = models.CharField(max_length=100, null=False, blank=False)
+    supplier = models.CharField(max_length=100, null=True, blank=True)
+
